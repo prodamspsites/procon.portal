8ebcac1654ac49875fd0d6dd609cd696850d5cf7
Raphael Iarussi <pigaov10@gmail.com>
2016-03-15 11:04:24 -0300

    Trata categorias consumidor gov

---
+++ src/procon/portal/browser/content/consumidor.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+import json
+from Products.Five import BrowserView
+from plone import api
+from plone.dexterity.utils import createContentInContainer
+from pymongo import MongoClient
+
+
+class Consumidor(BrowserView):
+    diretorio = "Consumidor"
+
+    def criaDiretorio(self):
+        """ cria diretorios para consumidor """
+        portal = api.portal.get()
+        if self.diretorio not in portal.objectIds():
+            self.montaEstrutura()
+        else:
+            del portal[self.diretorio]
+            self.montaEstrutura()
+
+    def consultaDiretorio(self):
+        """ consulta diretorios para consumidor """
+        self.consulta()
+
+    def montaEstrutura(self):
+            portal = api.portal.get()
+            portal.invokeFactory('Folder', self.diretorio)
+            consumidor = portal[self.diretorio]
+            print "criado diretorio" + self.diretorio
+            consumidor.reindexObject()
+            folder = portal[self.diretorio]
+            categorias = self.categorias()
+            for categoria in categorias:
+                nome = categoria['name']
+                url = categoria['url']
+                categoria = categoria['category']
+                print 'criado objeto de url' + url
+                createContentInContainer(folder,
+                                         'Link',
+                                         title=nome,
+                                         remoteUrl=url,
+                                         categoria=categoria
+                                         )
+
+    def categorias(self):
+        """ categorias do site consumidor.gov.br """
+        client = MongoClient()
+        db = client.consumidor
+
+        client = MongoClient()
+        db = client.consumidor
+        categorias = db.empresas.find()
+        return categorias
+
+    def consulta(self):
+        portal = api.portal.get()
+        catalog = portal.portal_catalog
+        links = catalog(portal_type="Link")
+        dados = {}
+        if links:
+            for i in links:
+                result = i.getObject()
+                dados[result.id] = {
+                    "url": result.remoteUrl,
+                    "categoria": result.categoria,
+                    "titulo": result.title
+                }
+                dados_to_json = json.dumps(dados, ensure_ascii=False)
+        print dados_to_json
+        print type(dados_to_json)
