cc5e4223dc58d8036b1b34003c1bb90a91eb866f
mosesjr <shank.sa@gmail.com>
2016-04-01 16:18:32 -0300

    Prepara estrutura para browserview de exibição dos envios de formulário

--- src/procon/portal/browser/content/reclamacao.py
+++ src/procon/portal/browser/content/reclamacao.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 from Products.Five import BrowserView
-
+from plone import api
 
 class Reclamacao(BrowserView):
 
     def buscaReclamacao(self):
-        cols = self.getColumnNames()
+    	portal = api.portal.get()
+    	folderConsumidor = portal['consumidor']
+    	form = folderConsumidor['formID']
+    	sendDataAdapter = form['adapterID']
+    	dados = sendDataAdapter.getSavedFormInputItems()
 
-        for row in self.getSavedFormInput():
-            print self.rowAsColDict(row, cols)
+    	dados = [x for x in dados if type(x) is list]
+
+    	# import pdb; pdb.set_trace()
+
+    	return dados
