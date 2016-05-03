cd5751a68775a867b3204c107713a0dc7f4274f4
Raphael Iarussi <pigaov10@gmail.com>
2016-05-03 17:26:58 -0300

    configuraçoes pergunta resposta

--- src/procon/portal/content.py
+++ src/procon/portal/content.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from procon.portal.interfaces import IPergunta
+from nfe.portal.interfaces import IPergunta, IResposta
+from plone.dexterity.content import Item
 from plone.dexterity.content import Container
 from zope.interface import implements
 from plone.indexer import indexer
@@ -13,9 +14,17 @@ class Pergunta(Container):
 
 @indexer(IPergunta)
 def searchableText(obj):
-    text = u' '.join([obj.pergunta.output])
-    text += u' '.join([obj.resposta.output])
+    return u' '.join([obj.pergunta.output])
 
-    return text
+grok.global_adapter(searchableText, name='SearchableText')
+
+
+class Resposta(Item):
+    implements(IResposta)
+
+
+@indexer(IPergunta)
+def searchableText(obj):
+    return ' '.join([obj.resposta.output])
 
 grok.global_adapter(searchableText, name='SearchableText')
--- src/procon/portal/indexers.py
+++ src/procon/portal/indexers.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from procon.portal.interfaces import IPergunta
+from nfe.portal.interfaces import IPergunta
+from nfe.portal.interfaces import IResposta
 from plone.indexer.decorator import indexer
 
 
 @indexer(IPergunta)
 def PerguntaSearchableIndex(obj):
-    text = u' '.join([obj.pergunta.output])
-    text += u' '.join([obj.resposta.output])
-    return text
+    return u' '.join([obj.pergunta.output])
+
+
+@indexer(IResposta)
+def RespostaSearchableIndex(obj):
+    return u' '.join([obj.resposta.output])
