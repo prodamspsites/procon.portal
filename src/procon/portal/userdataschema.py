5b60bb485b747d9a849df51e4953aefaccf5143c
Raphael Iarussi <pigaov10@gmail.com>
2016-05-17 13:43:41 -0300

    novos campos em register

--- src/procon/portal/userdataschema.py
+++ src/procon/portal/userdataschema.py
@@ -7,6 +7,7 @@ from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
 from z3c.form import field
 from z3c.form.browser.radio import RadioFieldWidget
+from z3c.form.browser.checkbox import CheckBoxFieldWidget
 
 from plone.supermodel import model
 from plone.app.users.browser.account import AccountPanelSchemaAdapter
@@ -37,6 +38,21 @@ estadocivil_options = SimpleVocabulary([
     SimpleTerm(value='divorciado', title=_(u'Divorciado(a)')),
     SimpleTerm(value='viuvo', title=_(u'Viúvo(a)')), ])
 
+adicionais_escolha_um = SimpleVocabulary([
+    SimpleTerm(value='sim', title=_(u'Sim')),
+    SimpleTerm(value='nao', title=_(u'Não')), ])
+
+adicionais_escolha_dois = SimpleVocabulary([
+    SimpleTerm(value='sim', title=_(u'Sim')),
+    SimpleTerm(value='nao', title=_(u'Não')), ])
+
+adicionais_escolha_tres = SimpleVocabulary([
+    SimpleTerm(value='sim', title=_(u'Sim')),
+    SimpleTerm(value='nao', title=_(u'Não')), ])
+
+termos = SimpleVocabulary([
+    SimpleTerm(value='sim', title=_(u'Eu concordo com os Termos de Uso do Consumidor.'))])
+
 
 def validateAccept(value):
     if value is not True:
@@ -48,6 +64,33 @@ class IEnhancedUserDataSchema(model.Schema):
     """Use all the fields from the default user data schema, and add various
     extra fields.
     """
+
+    termos_uso = schema.List(title=u"Termos de Uso do Consumidor.",
+                             # description=u"Eu concordo com os Termos de Uso do Consumidor.",
+                             required=True,
+                             value_type=schema.Choice(vocabulary=termos)
+                             )
+
+    adicional_um = schema.Choice(title=_(u'Possui mais de 60 anos ?'),
+                                 description=_(u'Possui mais de 60 anos ?'),
+                                 vocabulary=adicionais_escolha_um,
+                                 required=True,)
+
+    adicional_um = schema.Choice(title=_(u'Possui mais de 60 anos ?'),
+                                 description=_(u'Possui mais de 60 anos ?'),
+                                 vocabulary=adicionais_escolha_um,
+                                 required=True,)
+
+    adicional_tres = schema.Choice(title=_(u'Possui alguma deficiência ?'),
+                                   description=_(u'Possui alguma deficiência ?'),
+                                   vocabulary=adicionais_escolha_tres,
+                                   required=True,)
+
+    adicional_dois = schema.Choice(title=_(u'Portador de alguma doença grave prevista na Lei nº 12.008/09, atestada por declaração/laudo médico ?'),
+                                   description=_(u'Portador de alguma doença grave prevista na Lei nº 12.008/09, atestada por declaração/laudo médico ?'),
+                                   vocabulary=adicionais_escolha_dois,
+                                   required=True,)
+
     municipio = schema.Choice(
         title=_(u'1) O consumidor deve assinalar se possui ou não domicílio no Município de São Paulo.', default=u'1) O consumidor deve assinalar se possui ou não domicílio no Município de São Paulo.'),
         description=_(u'Você possui domicílio no Município de São Paulo?',
@@ -70,6 +113,7 @@ class IEnhancedUserDataSchema(model.Schema):
         description=_(u'',
                       default=u""),
         required=False,)
+
     nome_fantasia = schema.TextLine(
         title=_(u'Nome fantasia', default=u'Nome fantasia'),
         description=_(u'',
@@ -108,6 +152,66 @@ class IEnhancedUserDataSchema(model.Schema):
         vocabulary=estadocivil_options,
         required=False,)
 
+    data_nascimento = schema.TextLine(
+        title=_(u'Data de nascimento', default=u'Data de nascimento'),
+        required=True
+    )
+
+    contato_telefone = schema.TextLine(
+        title=_(u'DDD + Telefone/Celular'),
+        required=True
+    )
+
+    contato_celular = schema.TextLine(
+        title=_(u'DDD + Telefone/Celular'),
+        required=False
+    )
+
+    codigo_enderecamento_postal = schema.TextLine(
+        title=_(u'CEP', default=u'CEP'),
+        required=True
+    )
+
+    logradouro = schema.TextLine(
+        title=_(u'Logradouro'),
+        required=False
+    )
+
+    complemento = schema.TextLine(
+        title=_(u'Número/Complemento'),
+        required=True
+    )
+
+    bairro = schema.TextLine(
+        title=_(u'Bairro'),
+        required=True
+    )
+
+    cidade = schema.TextLine(
+        title=_(u'Cidade'),
+        required=False
+    )
+
+    unidade_federativa = schema.TextLine(
+        title=_(u'UF'),
+        required=False
+    )
+
+    site = schema.TextLine(
+        title=_(u'Site'),
+        required=False
+    )
+
+    mail = schema.TextLine(
+        title=_(u'E-mail'),
+        required=True
+    )
+
+    confirmacao = schema.TextLine(
+        title=_(u'Confirmação de E-mail'),
+        required=True
+    )
+
 
 class EnhancedUserDataSchemaAdapter(AccountPanelSchemaAdapter):
     schema = IEnhancedUserDataSchema
@@ -122,6 +226,10 @@ class UserDataPanelExtender(extensible.FormExtender):
         fields['tipo'].widgetFactory = RadioFieldWidget
         fields['municipio'].widgetFactory = RadioFieldWidget
         fields['genero'].widgetFactory = RadioFieldWidget
+        fields['adicional_um'].widgetFactory = RadioFieldWidget
+        fields['adicional_dois'].widgetFactory = RadioFieldWidget
+        fields['adicional_tres'].widgetFactory = RadioFieldWidget
+        fields['termos_uso'].widgetFactory = CheckBoxFieldWidget
         self.add(fields)
 
 
@@ -133,6 +241,11 @@ class RegistrationPanelExtender(extensible.FormExtender):
         fields['tipo'].widgetFactory = RadioFieldWidget
         fields['municipio'].widgetFactory = RadioFieldWidget
         fields['genero'].widgetFactory = RadioFieldWidget
+        fields['adicional_um'].widgetFactory = RadioFieldWidget
+        fields['adicional_dois'].widgetFactory = RadioFieldWidget
+        fields['adicional_tres'].widgetFactory = RadioFieldWidget
+        fields['termos_uso'].widgetFactory = CheckBoxFieldWidget
+
         self.add(fields)
 
 
@@ -144,6 +257,11 @@ class AddUserFormExtender(extensible.FormExtender):
         fields['tipo'].widgetFactory = RadioFieldWidget
         fields['municipio'].widgetFactory = RadioFieldWidget
         fields['genero'].widgetFactory = RadioFieldWidget
+        fields['adicional_um'].widgetFactory = RadioFieldWidget
+        fields['adicional_dois'].widgetFactory = RadioFieldWidget
+        fields['adicional_tres'].widgetFactory = RadioFieldWidget
+        fields['termos_uso'].widgetFactory = CheckBoxFieldWidget
+
         # management form doesn't need this field
         fields = fields.omit('accept')
         self.add(fields)
