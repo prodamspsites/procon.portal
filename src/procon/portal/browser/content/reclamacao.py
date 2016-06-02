ff4ace90b3ca25b12b0695c7c3be1dc814abdda1
mosesjr <shank.sa@gmail.com>
2016-06-02 13:19:56 -0300

    Atualiza templates de backoffice

--- src/procon/portal/browser/content/reclamacao.py
+++ src/procon/portal/browser/content/reclamacao.py
@@ -7,13 +7,33 @@ class Reclamacao(BrowserView):
 
     def buscaReclamacao(self):
         portal = api.portal.get()
-        folderConsumidor = portal['denuncia']
+        folderConsumidor = portal['consumidor']
+        form = folderConsumidor['formularios']
+        sendDataAdapter = form['dados']
+        dados = sendDataAdapter.getSavedFormInput()
+
+        dados = [x for x in dados if type(x) is list]
+
+        return dados
+
+    def buscaDenuncia(self):
+        portal = api.portal.get()
+        folderConsumidor = portal['consumidor']
         form = folderConsumidor['formulario-de-denuncia']
         sendDataAdapter = form['dados']
         dados = sendDataAdapter.getSavedFormInput()
 
         dados = [x for x in dados if type(x) is list]
 
-        # import pdb; pdb.set_trace()
+        return dados
+
+    def buscaFornecedor(self):
+        portal = api.portal.get()
+        folderConsumidor = portal['fornecedor']
+        form = folderConsumidor['adesao-ao-procon-paulistano']
+        sendDataAdapter = form['dados']
+        dados = sendDataAdapter.getSavedFormInput()
+
+        dados = [x for x in dados if type(x) is list]
 
         return dados
