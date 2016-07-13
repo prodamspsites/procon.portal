4f2ebd399cd1233e65e8c9289a7bac8770e5fda1
Raphael Iarussi <pigaov10@gmail.com>
2016-07-13 13:35:24 -0300

    configuração ambiente mongoDB

--- src/procon/portal/__init__.py
+++ src/procon/portal/__init__.py
@@ -10,8 +10,8 @@ hostname = socket.gethostbyname(socket.gethostname())
 
 if hostname == "127.0.0.1" or "10.20.26.20":
     MONGODB_HOSTS["host"] = "localhost"
-    MONGODB_HOSTS["port"] = "27017"
+    MONGODB_HOSTS["port"] = 27017
 
 elif hostname == "10.20.25.200":
     MONGODB_HOSTS["host"] = "mongo0.prodam"
-    MONGODB_HOSTS["port"] = "27017"
+    MONGODB_HOSTS["port"] = 27017
